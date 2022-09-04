f2770b243ec468e09b732e8bc5ab5a96819ae0ba
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-09-04 22:42:02 +0600

    mongdb connect

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Pass}@cluster0.eihnvmx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("mongo connect");
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
